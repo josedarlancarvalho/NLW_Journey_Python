84cb56354b37a4ef25690b971ced5749c81ce9fd
josedarlancarvalho <carvalhodarlan38@gmail.com>
2024-07-09 16:55:57 -0300

    src/models/repositories/links_repository_test.py

--- src/models/repositories/links_repository_test.py
+++ src/models/repositories/links_repository_test.py
@@ -8,6 +8,7 @@ db_connection_handler.connect()
 link_id = str(uuid.uuid4())
 trip_id = str(uuid.uuid4())
 
+@pytest.mark.skip(reason="interacao com o banco")
 def test_registry_link():
     conn = db_connection_handler.get_connection()
     link_repository = LinksRepository(conn)
@@ -20,3 +21,13 @@ def test_registry_link():
     }
 
     link_repository.registry_link(link_infos)
+
+@pytest.mark.skip(reason="interacao com o banco")
+def test_find_links_from_trip():
+    conn = db_connection_handler.get_connection()
+    link_repository = LinksRepository(conn)
+
+    response = link_repository.find_links_from_trip(trip_id)
+    
+    assert isinstance(response, list)
+    assert isinstance(response[0], tuple)
