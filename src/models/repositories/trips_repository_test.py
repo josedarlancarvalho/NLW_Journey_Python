499001c0468fcbf7d2f6321a56f4d80ad86e35ad
josedarlancarvalho <carvalhodarlan38@gmail.com>
2024-07-09 11:27:40 -0300

    implementando emails repository

--- src/models/repositories/trips_repository_test.py
+++ src/models/repositories/trips_repository_test.py
@@ -1,3 +1,4 @@
+import pytest
 import uuid
 from datetime import datetime, timedelta
 from .trips_repository import TripsRepository
@@ -5,6 +6,8 @@ from src.models.settings.db_connection_handler import db_connection_handler
 
 db_connection_handler.connect()
 trip_id = str(uuid.uuid4())
+
+@pytest.mark.skip(reason="interacao com o banco")
 def test_create_trip():
     conn = db_connection_handler.get_connection()
     trips_repository = TripsRepository(conn)
@@ -20,6 +23,7 @@ def test_create_trip():
 
     trips_repository.create_trip(trips_info)
 
+@pytest.mark.skip(reason="interacao com o banco")
 def test_find_trip_by_id():
     conn = db_connection_handler.get_connection()
     trips_repository = TripsRepository(conn)
@@ -27,6 +31,7 @@ def test_find_trip_by_id():
     trip = trips_repository.find_trip_by_id(trip_id)
     print(trip)
 
+@pytest.mark.skip(reason="interacao com o banco")
 def test_update_trip_status():
     conn = db_connection_handler.get_connection()
     trips_repository = TripsRepository(conn)
